69266d85c33dd4c70f39f3dddf4556ef930dbd25
Saša Savić <sasa.savic@mail.ru>
2015-04-27 22:05:09 +0200

    Change the default python interpreter to python3

--- init.el
+++ init.el
@@ -270,12 +270,17 @@
 (key-chord-define-global "qj" 'ace-jump-mode)
 (key-chord-define-global "ql" 'ace-jump-line-mode)
 (key-chord-define-global "qc" 'ace-jump-char-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; VARS ADDED BY CONFIG UTILITY ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(send-mail-function (quote smtpmail-send-it)))
+ '(python-shell-interpreter "python3")
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
