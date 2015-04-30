924fafaa6eec6cdd9119be31963697f5a9a6cd4b
Saša Savić <sasa.savic@mail.ru>
2015-04-30 11:46:37 +0200

    Removed color themes

--- init.el
+++ init.el
@@ -47,8 +47,7 @@
    php-mode-improved        ; Basic mode for PHP
    markdown-mode            ; Markdown is a must these days
    emacs-powerline          ; Powerline mode bar for emacs
-   color-theme		        ; nice looking emacs
-   color-theme-tango))      ; check out color-theme-solarized
+   ))
 
 ;;
 ;; Some recipes require extra tools to be installed
@@ -284,9 +283,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(python-shell-interpreter "python3")
-(custom-set-faces
+ (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+)
