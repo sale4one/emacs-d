5bddb36eef2326b2161f4de138774d59c74a821d
Saša Savić <savic-sasa@outlook.com>
2015-07-29 15:07:26 +0200

    Removed local recepies, fixed minor issues

--- init.el
+++ init.el
@@ -7,49 +7,31 @@
   (with-current-buffer
       (url-retrieve-synchronously
        "https://github.com/dimitri/el-get/raw/master/el-get-install.el")
-    (end-of-buffer)
+    (goto-char (point-max))
     (eval-print-last-sexp)))
 
 ;; now either el-get is `require'd already, or have been `load'ed by the
 ;; el-get installer.
 
-;; set local recipes
-(setq
- el-get-sources
- '((:name buffer-move			; have to add your own keys
-	  :after (progn
-		   (global-set-key (kbd "<C-S-up>")     'buf-move-up)
-		   (global-set-key (kbd "<C-S-down>")   'buf-move-down)
-		   (global-set-key (kbd "<C-S-left>")   'buf-move-left)
-		   (global-set-key (kbd "<C-S-right>")  'buf-move-right)))
-
-   (:name smex				; a better (ido like) M-x
-	  :after (progn
-		   (setq smex-save-file "~/.emacs.d/.smex-items")
-		   (global-set-key (kbd "M-x") 'smex)
-		   (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
-
-   (:name goto-last-change		; move pointer back to last change
-	  :after (progn
-		   ;; when using AZERTY keyboard, consider C-x C-_
-		   (global-set-key (kbd "C-x C-/") 'goto-last-change)))))
-
 ;; now set our own packages
 (setq
  my:el-get-packages
- '(el-get		    ; el-get is self-hosting
-   switch-window	    ; takes over C-x o
-   auto-complete	    ; complete as you type with overlays
+ '(el-get		            ; el-get is self-hosting
+   buffer-move              ; throw buffers around
+   smex                     ; better M-x
+   goto-last-change         ; moves pointer to last change
+   switch-window	        ; takes over C-x o
+   auto-complete	        ; complete as you type with overlays
    key-chord                ; make simple keybinds with any button
    ace-jump-mode            ; jump around the buffer easily
-   expand-region            ; Expands selection to  quotes, stetements, blocks, ...
-   python-mode              ; Something to help editing python files a bit
-   jedi                     ; Jedi based autocompletion for python files
-   web-mode                 ; Handy for editing web page templates
-   php-mode-improved        ; Basic mode for PHP
-   markdown-mode            ; Markdown is a must these days
-   clojure-mode             ; Edit clojure files in emacs
-   cider                    ; Clojure IDE and REPL integration
+   expand-region            ; expands selection to  quotes, stetements, blocks, ...
+   python-mode              ; something to help editing python files a bit
+   jedi                     ; jedi based autocompletion for python files
+   web-mode                 ; handy for editing web page templates
+   php-mode-improved        ; basic mode for PHP
+   markdown-mode            ; markdown is a must these days
+   clojure-mode             ; edit clojure files in emacs
+   cider                    ; clojure IDE and REPL integration
    ))
 
 ;;
@@ -82,6 +64,9 @@
 ;; Setting for emacs-jedi
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
+
+;; Smex setup
+(setq smex-save-file "~/.emacs.d/.smex-items")
 
 
 
@@ -277,6 +262,13 @@
 (global-set-key (kbd "<M-down>")   'buf-move-down)
 (global-set-key (kbd "<M-left>")   'buf-move-left)
 (global-set-key (kbd "<M-right>")  'buf-move-right)
+
+;; Smex
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; goto-last-change
+(global-set-key (kbd "C-c l") 'goto-last-change)
 
 
 
